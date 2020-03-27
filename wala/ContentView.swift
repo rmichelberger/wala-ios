143133c7d7c8ace1f21c0f529aa783896f26ce1d
Roland Michelberger <rmichelberger@gmail.com>
2020-03-28 00:25:40 +0100

    Initial Commit

---
+++ wala/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wala
+//
+//  Created by Roland Michelberger on 28.03.20.
+//  Copyright © 2020 makeitappen GmbH. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
